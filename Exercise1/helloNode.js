86037a4bdeb0f98d71f0f4261ef333be672a7786
Miguyy <miguelangelo123machado@gmail.com>
2026-03-08 15:26:30 +0000

    test(node): Testing the first node.js exercise.

---
+++ Exercise1/helloNode.js
@@ -0,0 +1,35 @@
+//console.log("Hello Node");
+
+//console.log(process.argv.slice(2));
+
+//console.log(process.argv.slice.PORT);
+
+//node basic server
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  /* res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!"); */
+  const { method, url } = req;
+  console.log(`Received ${method} request for ${url}`);
+
+  switch (url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Welcome to the Home Page!");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("This is the About Page.");
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
